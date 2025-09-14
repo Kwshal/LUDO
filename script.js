de724d330c10e26ec8a72cac81d28c60a1f6084c
kwshal <kushal3738@gmail.com>
2025-09-15 01:34:14 +0530

    auto play, capture logic,

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const board = document.getElementById("board");
 const tokens = Array.from(document.querySelectorAll(".token"));
 const moveSquares = Array.from(document.querySelectorAll(".move-square"));
-// console.log(moveSquares);
-// const homeSquares = [...document.querySelectorAll(".homeSquare")];
+const homeSquares = Array.from(document.querySelectorAll(".home-square"));
+document.querySelectorAll(".final-arrow, .token").forEach(el => {
+     el.style.width = `min(${el.parentElement.offsetWidth}px, ${el.parentElement.offsetHeight}px)`;
+});
 const currentPlayerElement = document.getElementById("current-player");
 
 let dice = document.getElementById("dice");
@@ -10,6 +12,7 @@ let diceNumber = null;
 let currentPlayer = "blue";
 let tokenHasMove = true; //! add condition
 let diceRollable = true;
+let extraTurn = false;
 let turnDone = false;
 let x = -1;
 let tokenIsColored = false;
@@ -26,6 +29,7 @@ dice.addEventListener("click", () => {
           diceNumber = Math.floor(Math.random() * 6 + 1);
           dice.innerText = diceNumber;
           diceRollable = false;
+          extraTurn = false;
           turnDone = false;
           if (!someOpenToken()) {
                if (diceNumber !== 6) {
@@ -37,9 +41,8 @@ dice.addEventListener("click", () => {
                          updatePlayer(++x);
                     }, 600);
                }
-               // turnDone && updatePlayer(++x);
-               // console.log(currentPlayer,"d:", diceNumber);
           }
+          autoMove();
      }
 });
 function someOpenToken() {
@@ -61,14 +64,17 @@ board.addEventListener("click", e => {
 
           if (diceNumber !== 6) {
                if (tokenIsOpen && tokenHasMove) {
+                    captureToken(nextSquare);
                     nextSquare.appendChild(token);
                     turnDone = true;
                     diceRollable = true;
                     diceNumber = "#";
                     dice.innerHTML = "#"
+                    token.style.scale = ".5";
                } else diceRollable = false;
           } else {
                if (tokenIsOpen && tokenHasMove) {
+                    captureToken(nextSquare);
                     nextSquare.appendChild(token);
                     diceRollable = true;
                } else {
@@ -79,26 +85,74 @@ board.addEventListener("click", e => {
                turnDone = false;
                diceNumber = "#";
                dice.innerHTML = "#"
+               token.style.scale = ".5";
           }
-          turnDone && updatePlayer(++x);
+          !extraTurn && turnDone && updatePlayer(++x);
      }
 });
 
 function updatePlayer(x) {
      currentPlayer = ["blue", "yellow", "green", "pink"][x % 4];
-     // console.log(currentPlayer, "d", diceNumber);
      currentPlayerElement.innerText = currentPlayer.toUpperCase();
+     priortiseTokens(tokens.filter(token => token.classList.contains(`token-${currentPlayer}`)));
 }
 
-// function openToken(token, homeSquare) {
 
+// let colors = {
+//      blue: "blue",
+//      yellow: "yellow",
+//      green: "green",
+//      pink: "pink"
 // }
+const indicator = document.createElement("span");
+indicator.className = "indicator";
 
-// 1 to 17 instead of 1 to 6????? edit: bad idea.
+function priortiseTokens(currentTokens) {
+     tokens.forEach(token => {
+          token.style.zIndex = "1";
+          token.style.boxShadow = "0 1px 2px gray"
 
+          // token.innerHTML = "";
+          // token.style.border = "none";
+     });
+     currentTokens.forEach(token => {
+          token.style.zIndex = "2";
+          token.style.boxShadow = "0 2px 3px gray";
 
+          // token.style.border = "1px solid";
+          // token.appendChild(indicator.cloneNode(true));
+     });
+}
 
+function captureToken(square) {
+     if (!square.classList.contains(".stop")) {
+          let token = square.querySelector(".token");
+          if (token) {
+               token.style.scale = "1.7";
+               token.classList.remove("open");
+               let homeSquare = document.querySelector(`.home-square-${token.dataset.color}`)
+               homeSquare.appendChild(token);
+               extraTurn = true;
+          }
+     }
+}
 
-document.querySelectorAll(".final-arrow, .token").forEach(el => {
-     el.style.width = `min(${el.parentElement.offsetWidth}px, ${el.parentElement.offsetHeight}px)`;
-})
+function autoMove() {
+     let tokens = Array.from(document.querySelectorAll(`.token-${currentPlayer}`));
+     let allTokensClosed = tokens.every(token => !token.classList.contains("open"));
+     let openTokens = tokens.filter(token => token.classList.contains("open"));
+     let onlyToken = openTokens.length === 1;
+     let tokensAtSamePlace = openTokens.length > 0 && openTokens.every(token => token.parentElement.classList.contains(`${currentPlayer}-stop`));
+
+     setTimeout(() => {
+          
+     if ((onlyToken || tokensAtSamePlace) && diceNumber !== 6) {
+          openTokens[0].click();
+     }
+     if (allTokensClosed && diceNumber === 6) {
+          tokens[0].click();
+     }
+     }, 600);
+}
+
+//? colored tokens can just have their .token removed.
