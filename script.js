364cf3bc2ea311cccf534add7fe7698c03ed5576
kwshal <kushal3738@gmail.com>
2025-09-19 16:30:44 +0530

    improve UI, token animation.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let currentPlayer = "blue";
 let diceRollable = true;
 let extraTurn = false;
 let turnDone = false;
+
 let x = -1;
 let tokenIsColored = false;
 let tokensColored = {
@@ -24,26 +25,26 @@ let tokensColored = {
 
 dice.addEventListener("click", () => {
      if (diceRollable) {
-          !(diceNumber === "#" || diceNumber === 6) && updatePlayer(++x); //? ++ (post increment operator)
+          // console.log(diceRollable);
+          diceNumber === null && updatePlayer(++x) && console.log("dice"); //? ++ (post increment operator)
           diceNumber = Math.floor(Math.random() * 6 + 1);
           dice.innerText = diceNumber;
           diceRollable = false;
-          extraTurn = false;
           turnDone = false;
+          extraTurn = false;
           if (!someMovableToken()) {
                if (diceNumber !== 6) {
                     setTimeout(() => {
                          turnDone = true;
-                         diceRollable = true;
-                         diceNumber = "#";
-                         dice.innerHTML = "#";
-                         updatePlayer(++x);
+                         updatePlayer(++x) && console.log("auto");
+                         initDice();
                     }, 600);
                }
           }
           autoMove();
      }
 });
+
 function someMovableToken() {
      let currentTokens = [...document.querySelectorAll(`.token-${currentPlayer}`)];
      return currentTokens.some(token => {
@@ -51,83 +52,80 @@ function someMovableToken() {
      });
 }
 
+function initDice() {
+     // !extraTurn && updatePlayer(++x) && console.log("handle");
+     diceRollable = true;
+     dice.innerHTML = "#";
+     diceNumber = "#";
+}
+
 board.addEventListener("click", e => {
      if (e.target.classList.contains("token") && e.target.classList.contains(`token-${currentPlayer}`)) {
           let token = e.target;
-          let tokenHasMove = token.classList.contains("open") && (+token.dataset.squaresMoved + diceNumber < 59);
-          let startSquare = document.querySelector(`.${currentPlayer}-stop`);
-          let currentSquare = +token.parentElement.dataset.sqNum;
-          let nextSquare = moveSquares.find(sq => +sq.dataset.sqNum === (currentSquare + diceNumber) % 52);
-          let squaresMoved = +token.dataset.squaresMoved;
-          let finalSquare = Array.from(document.querySelectorAll(`.final-square-${currentPlayer}`)).find(sq => +sq.dataset.sqNum === (squaresMoved + diceNumber - 52));
-
-          if (diceNumber === "#") return;
-          if (squaresMoved + diceNumber > 58) return;
-          
-          if ((squaresMoved + diceNumber) <= 52) {
-               if (diceNumber !== 6) {
-                    if (tokenHasMove) {
-                         captureToken(nextSquare);
-                         nextSquare.appendChild(token);
-                         turnDone = true;
-                         diceRollable = true;
-                         token.dataset.squaresMoved = `${squaresMoved + diceNumber}`;
-                         diceNumber = "#";
-                         dice.innerHTML = "#";
-                         token.style.scale = ".5";
-                    }
-               } else {
-                    if (tokenHasMove) {
-                         token.dataset.squaresMoved = `${squaresMoved + diceNumber}`;
-                         captureToken(nextSquare);
-                         nextSquare.appendChild(token);
-                         diceRollable = true;
-                    } else {
-                         token.dataset.squaresMoved = `${squaresMoved + 1}`;
-                         startSquare.appendChild(token);
-                         token.classList.add("open");
-                         diceRollable = true;
-                    }
-                    turnDone = false;
-                    extraTurn = true;
-                    currentPlayerElement.innerHTML = currentPlayer.toUpperCase() + " ↻";
-                    diceNumber = "#";
-                    dice.innerHTML = "#"
-                    token.style.scale = ".5";
-               }
-               // console.log(token.dataset.squaresMoved);
-          }
-          else if ((squaresMoved + diceNumber) > 52 && (squaresMoved + diceNumber) < 58) {
-               finalSquare.appendChild(token);
-               turnDone = true;
-               diceRollable = true;
-               token.dataset.squaresMoved = `${squaresMoved + diceNumber}`;
-               diceNumber = "#";
-               dice.innerHTML = "#";
-          }
-          else if ((squaresMoved + diceNumber) === 58) {
-               token.classList.add("colored");
-               token.classList.remove(`token-${currentPlayer}`);
-               token.style.scale = "1.7";
-               tokenIsColored[currentPlayer] += 1;
-               let placeSquare = Array.from(document.querySelectorAll(`.home-square-${token.dataset.color} .place-square`)).find(sq => !sq.querySelector(".token"));
-               placeSquare.appendChild(token);
-               token.innerHTML = "✪";
-               diceNumber = "#";
-               dice.innerHTML = "#";
-               diceRollable = true;
-               extraTurn = true;
-               currentPlayerElement.innerHTML = currentPlayer.toUpperCase() + " ↻";
-               checkWinner();
-          }
-          !extraTurn && turnDone && updatePlayer(++x);
+          let tokenHasMove = ((!token.classList.contains("open") && diceNumber === 6) || token.classList.contains("open")) && (+token.dataset.squaresMoved + diceNumber < 59) && diceNumber !== "#" && diceNumber !== "✦";
+          tokenHasMove && moveToken(token);
      }
 });
+
+function colorToken(token) {
+     let placeSquare = Array.from(document.querySelectorAll(`.home-square-${token.dataset.color} .place-square`)).find(sq => !sq.querySelector(".token"));
+     placeSquare.appendChild(token);
+     token.classList.add("colored");
+     token.classList.remove(`token-${currentPlayer}`);
+     token.style.scale = "1.7";
+     console.log("here");
+     token.innerHTML = "✪";
+     tokenIsColored[currentPlayer] += 1;
+     currentPlayerElement.innerHTML = currentPlayer.toUpperCase() + " ↻";
+     checkWinner();
+}
+
+async function moveToken(token) {
+
+     let startSquare = document.querySelector(`.${currentPlayer}-stop`);
+     let currentSquare = +token.parentElement.dataset.sqNum;
+     let squaresMoved = +token.dataset.squaresMoved;
+     let moveSquare = (i) => moveSquares.find(sq => +sq.dataset.sqNum === (currentSquare + i) % 52);
+     let finalSquare = (i) => Array.from(document.querySelectorAll(`.final-square-${currentPlayer}`)).find(sq => +sq.dataset.sqNum === (squaresMoved + i - 52));
+
+     extraTurn = diceNumber === 6;
+
+     for (let i = 1; i <= diceNumber; i++) {
+          // console.log(token.dataset.squaresMoved, "a");
+
+          if (squaresMoved === 0 && diceNumber === 6) {
+               startSquare.appendChild(token);
+               token.style.scale = ".5";
+               // console.log("here");
+               token.classList.add("open");
+               diceNumber = 1;
+               break;
+          } else if (squaresMoved + i < 53) {
+               i === diceNumber && captureToken(moveSquare(i)) && (extraTurn = true);
+               moveSquare(i).appendChild(token);
+               await new Promise(resolve => setTimeout(resolve, 250));
+          } else if (squaresMoved + i === 58) {
+               colorToken(token);
+               extraTurn = true;
+          } else {
+               finalSquare(i).appendChild(token);
+               await new Promise(resolve => setTimeout(resolve, 250));
+          }
+     }
+     token.dataset.squaresMoved = `${squaresMoved + diceNumber}`;
+     // console.log(token.dataset.squaresMoved);
+     !extraTurn && updatePlayer(++x) && console.log("move");
+     initDice();
+     // console.log(diceRollable);
+}
+
 
 function updatePlayer(x) {
      currentPlayer = ["blue", "yellow", "green", "pink"][x % 4];
      currentPlayerElement.innerText = currentPlayer.toUpperCase();
      priortiseTokens(tokens.filter(token => token.classList.contains(`token-${currentPlayer}`)));
+     // console.log("switched");
+     return true;
 }
 
 function priortiseTokens(currentTokens) {
@@ -146,13 +144,15 @@ function captureToken(square) {
      let token = square.querySelector(".token");
      if (token) {
           token.style.scale = "1.7";
+          console.log("here");
           token.classList.remove("open");
           let placeSquare = Array.from(document.querySelectorAll(`.home-square-${token.dataset.color} .place-square`)).find(sq => !sq.querySelector(".token"));
           placeSquare.appendChild(token);
-          extraTurn = true;
           currentPlayerElement.innerHTML = currentPlayer.toUpperCase() + " ↻";
           token.dataset.squaresMoved = "0";
+          return true;
      }
+     return false;
 }
 
 function autoMove() {
@@ -162,13 +162,13 @@ function autoMove() {
      let onlyOpenToken = openTokens.length === 1;
      let onlyMovableToken = getMovableTokens(openTokens).length === 1;
      let tokensAtSamePlace = openTokens.length > 0 && openTokens.every(token => token.parentElement.classList.contains(`${currentPlayer}-stop`));
-
+     let r = Math.floor(Math.random() * 4);
      setTimeout(() => {
           if ((onlyOpenToken || tokensAtSamePlace || onlyMovableToken) && diceNumber !== 6) {
                openTokens[0].click();
           }
           if (allTokensClosed && diceNumber === 6) {
-               tokens[0].click();
+               tokens[r].click();
           }
      }, 600);
 }
@@ -189,16 +189,4 @@ function checkWinner() {
      extraTurn = false;
 }
 
-async function animateMoves(currentSquare, squaresMoved) {
-     for (let i = 1; i < diceNumber+1; i++) {
-          let nextSquare = moveSquares[(currentSquare + i) % 52];
-          setTimeout(() => {
-               if ((squaresMoved + i) < 52) {
-                    
-               }
-
-          }, 250);
-          (currentSquare+diceNumber === squaresMoved) && (diceRollable = true);
-     }
-}
 
